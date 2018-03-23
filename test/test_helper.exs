885513bc20452b8477e656ffdf4e346af77b5f5d
Tallys Martins <tallysmartins@gmail.com>
2018-03-23 17:58:58 -0300

    Adds some tests to ElixirBench.Schema
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,20 @@ ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(ElixirBench.Repo, :manual)
 
+defmodule ElixirBenchWeb.AbsintheHelpers do
+  def query_skeleton(query) do
+    %{
+      "query" => "query { #{query} }",
+      "variables" => "{}"
+    }
+  end
+
+  def mutation_skeleton(query) do
+    %{
+      "operationName" => "",
+      "query" => "#{query}",
+      "variables" => ""
+    }
+  end
+end
+
