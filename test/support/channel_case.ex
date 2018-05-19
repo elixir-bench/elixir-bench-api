1eabb183058a08e43c141e527ba53e89fe1a661c
Tallys Martins <tallysmartins@gmail.com>
2018-05-19 13:16:19 -0300

    Apply mix format to tests
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -25,13 +25,13 @@ defmodule ElixirBenchWeb.ChannelCase do
     end
   end
 
-
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(ElixirBench.Repo)
+
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(ElixirBench.Repo, {:shared, self()})
     end
+
     :ok
   end
-
 end
