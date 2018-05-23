0334b0d8ad958c31d035da0183e3c65fcd4b085a
Tallys Martins <tallysmartins@gmail.com>
2018-05-23 18:30:15 -0300

    Adds more tests
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- test/elixir_bench/benchmarks/benchmarks_test.exs
+++ test/elixir_bench/benchmarks/benchmarks_test.exs
@@ -1,17 +1,41 @@
 defmodule ElixirBench.BenchmarksTest do
   use ElixirBench.DataCase
   alias ElixirBench.{Benchmarks, Benchmarks.Job}
-  alias ElixirBench.Repos
+
   import ElixirBenchWeb.TestHelpers
   import ElixirBench.Factory
 
   describe "create_job/2" do
-    test "return a new Job given correct params" do
+    test "insert new Job given correct params" do
       repo = insert(:repo)
 
       assert_difference(Job, 1) do
         Benchmarks.create_job(repo, %{branch_name: "mm/benche", commit_sha: "ABC123"})
       end
     end
+
+    test "return new Job inserted" do
+      repo = insert(:repo)
+
+      {:ok, job} = Benchmarks.create_job(repo, %{branch_name: "mm/benche", commit_sha: "ABC123"})
+      assert %Job{} = job
+    end
+
+    test "return error when missing job attributes" do
+      repo = build(:repo)
+
+      {:error, changeset} = Benchmarks.create_job(repo, %{})
+
+      refute changeset.valid?
+    end
+
+    test "raise an error when repository is not valid" do
+      repo = build(:repo)
+
+      assert_raise Postgrex.Error, fn ->
+        Benchmarks.create_job(repo, %{branch_name: "mm/benche", commit_sha: "ABC123"})
+      end 
+    end
   end
+
 end
