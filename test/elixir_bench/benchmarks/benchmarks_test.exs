a39b9b1761656fed843725ff07648bb56882e97d
Tallys Martins <tallysmartins@gmail.com>
2018-05-27 10:57:00 -0300

    Adds more tests to Benchmarks module
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- test/elixir_bench/benchmarks/benchmarks_test.exs
+++ test/elixir_bench/benchmarks/benchmarks_test.exs
@@ -1,10 +1,13 @@
 defmodule ElixirBench.BenchmarksTest do
   use ElixirBench.DataCase
-  alias ElixirBench.{Benchmarks, Benchmarks.Job, Benchmarks.Runner, Repos}
-  alias Benchmarks.{Benchmark, Measurement}
+  alias ElixirBench.{Benchmarks, Repos}
+  alias Benchmarks.{Benchmark, Measurement, Job, Runner}
 
   import ElixirBenchWeb.TestHelpers
   import ElixirBench.Factory
+
+  # number of samples in executed_job_fixture()
+  @samples_count 4
 
   describe "create_runner/1" do
     test "insert new Runner given correct params" do
@@ -33,8 +36,8 @@ defmodule ElixirBench.BenchmarksTest do
     test "return error when invalid credentials" do
       {:ok, _} = Benchmarks.create_runner(%{name: "myrunner", api_key: "mykey"})
 
-      assert {:error, :not_found} = Benchmarks.authenticate_runner("myrunner", "anotherkey")
-      assert {:error, :not_found} = Benchmarks.authenticate_runner("", "")
+      assert {:error, :not_found} == Benchmarks.authenticate_runner("myrunner", "anotherkey")
+      assert {:error, :not_found} == Benchmarks.authenticate_runner("", "")
     end
   end
 
@@ -49,8 +52,8 @@ defmodule ElixirBench.BenchmarksTest do
     test "return error if benchmark not found" do
       %{id: rid} = insert(:repo)
 
-      assert {:error, :not_found} = Benchmarks.fetch_benchmark(rid, "")
-      assert {:error, :not_found} = Benchmarks.fetch_benchmark(nil, "")
+      assert {:error, :not_found} == Benchmarks.fetch_benchmark(rid, "")
+      assert {:error, :not_found} == Benchmarks.fetch_benchmark(nil, "")
     end
   end
 
@@ -63,8 +66,8 @@ defmodule ElixirBench.BenchmarksTest do
     end
 
     test "return error if measurement not found" do
-      assert {:error, :not_found} = Benchmarks.fetch_measurement(nil)
-      assert {:error, :not_found} = Benchmarks.fetch_measurement(9999)
+      assert {:error, :not_found} == Benchmarks.fetch_measurement(nil)
+      assert {:error, :not_found} == Benchmarks.fetch_measurement(9999)
     end
   end
 
@@ -77,8 +80,8 @@ defmodule ElixirBench.BenchmarksTest do
     end
 
     test "return error if job not found" do
-      assert {:error, :not_found} = Benchmarks.fetch_job(nil)
-      assert {:error, :not_found} = Benchmarks.fetch_job(9999)
+      assert {:error, :not_found} == Benchmarks.fetch_job(nil)
+      assert {:error, :not_found} == Benchmarks.fetch_job(9999)
     end
   end
 
@@ -93,8 +96,8 @@ defmodule ElixirBench.BenchmarksTest do
     test "return error if job not found" do
       binary_uuid = "99999999-9999-9999-9999-999999999999"
 
-      assert {:error, :not_found} = Benchmarks.fetch_job_by_uuid(nil)
-      assert {:error, :not_found} = Benchmarks.fetch_job_by_uuid(binary_uuid)
+      assert {:error, :not_found} == Benchmarks.fetch_job_by_uuid(nil)
+      assert {:error, :not_found} == Benchmarks.fetch_job_by_uuid(binary_uuid)
     end
   end
 
@@ -120,6 +123,67 @@ defmodule ElixirBench.BenchmarksTest do
     test "return error when repo is not on databse" do
       assert_raise Postgrex.Error, fn ->
         Benchmarks.create_job(%Repos.Repo{}, %{branch_name: "mm/benche", commit_sha: "ABC123"})
+      end
+    end
+  end
+
+  describe "claim_job/1" do
+    test "delegate pending job to registered runner" do
+      %{id: jid, claimed_by: nil} = insert(:job)
+      %{id: rid} = runner = insert(:runner)
+
+      assert {:ok, job} = Benchmarks.claim_job(runner)
+      assert %Job{id: ^jid, claimed_by: ^rid} = job
+    end
+
+    test "return error if there is no pending jobs" do
+      runner = insert(:runner)
+
+      assert {:error, :not_found} == Benchmarks.claim_job(runner)
+    end
+
+    test "return error given invalid runner" do
+      assert {:error, :not_found} = Benchmarks.claim_job(%Runner{})
+      assert {:error, :not_found} = Benchmarks.claim_job(%Runner{id: 9999})
+    end
+  end
+
+  describe "submit_job/2" do
+    test "save benchmark and measurement given correct results" do
+      %{completed_at: nil} = job = insert(:job)
+
+      assert_difference(Benchmarks.Benchmark, @samples_count) do
+        assert_difference(Benchmarks.Measurement, @samples_count) do
+          assert :ok == Benchmarks.submit_job(job, executed_job_fixture())
+        end
+      end
+
+      job = ElixirBench.Repo.get(Job, job.id)
+      refute job.completed_at == nil
+    end
+
+    test "associate benchmarks to the given repo" do
+      %{repo: repo} = job = insert(:job)
+
+      Benchmarks.submit_job(job, executed_job_fixture())
+
+      query = Ecto.Query.where(Benchmark, repo_id: ^repo.id)
+      assert @samples_count == ElixirBench.Repo.aggregate(query, :count, :id)
+    end
+
+    test "do not duplicata benchmark in multiple runs" do
+      job = insert(:job)
+
+      assert_difference(Benchmarks.Benchmark, 4) do
+        assert_difference(Benchmarks.Measurement, 4) do
+          assert :ok == Benchmarks.submit_job(job, executed_job_fixture())
+        end
+      end
+
+      assert_difference(Benchmarks.Benchmark, 0) do
+        assert_difference(Benchmarks.Measurement, 4) do
+          assert :ok == Benchmarks.submit_job(job, executed_job_fixture())
+        end
       end
     end
   end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,15 @@ defmodule ElixirBench.Factory do
   use ExMachina.Ecto, repo: ElixirBench.Repo
 
   alias ElixirBench.Repos
-  alias ElixirBench.Benchmarks.{Benchmark, Measurement, Job}
+  alias ElixirBench.Benchmarks.{Benchmark, Measurement, Job, Runner}
+
+  def runner_factory do
+    %Runner{
+      name: "myrunner",
+      api_key: "mykey",
+      api_key_hash: Bcrypt.hash_pwd_salt("mykey")
+    }
+  end
 
   def repo_factory do
     %Repos.Repo{
@@ -59,6 +67,80 @@ defmodule ElixirBench.Factory do
       std_dev_ips: 1351.5088377210595,
       run_times: [1.87, 1.44],
       percentiles: %{"50" => 377.0, "99" => 578.6900000000005}
+    }
+  end
+
+  # This is the kind of data returned by benchee run. This sample has 4
+  # benchmarks examples
+  def executed_job_fixture() do
+    %{
+      "elixir_version" => "1.5.2",
+      "erlang_version" => "20.1",
+      "dependency_versions" => %{},
+      "cpu" => "Intel(R) Core(TM) i7-4770HQ CPU @ 2.20GHz",
+      "cpu_count" => 8,
+      "memory_mb" => 16384,
+      "log" => """
+      [now] Oh how ward it was to run this benchmark!
+      """,
+      "measurements" => %{
+        "insert_mysql/insert_plain" => %{
+          "average" => 393.560253365004,
+          "ips" => 2540.906993147397,
+          "maximum" => 12453.0,
+          "median" => 377.0,
+          "minimum" => 306.0,
+          "mode" => 369.0,
+          "percentiles" => %{"50" => 377.0, "99" => 578.6900000000005},
+          "sample_size" => 12630,
+          "std_dev" => 209.33476197004862,
+          "std_dev_ips" => 1351.5088377210595,
+          "std_dev_ratio" => 0.5319001605985423,
+          "run_times" => []
+        },
+        "insert_mysql/insert_changeset" => %{
+          "average" => 450.2023723288664,
+          "ips" => 2221.2233019276814,
+          "maximum" => 32412.0,
+          "median" => 397.0,
+          "minimum" => 301.0,
+          "mode" => 378.0,
+          "percentiles" => %{"50" => 397.0, "99" => 1003.3999999999942},
+          "sample_size" => 11044,
+          "std_dev" => 573.9417528830307,
+          "std_dev_ips" => 2831.732735787863,
+          "std_dev_ratio" => 1.274852795453007,
+          "run_times" => []
+        },
+        "insert_pg/insert_plain" => %{
+          "average" => 473.0912894636744,
+          "ips" => 2113.756947699591,
+          "maximum" => 13241.0,
+          "median" => 450.0,
+          "minimum" => 338.0,
+          "mode" => 442.0,
+          "percentiles" => %{"50" => 450.0, "99" => 727.0},
+          "sample_size" => 10516,
+          "std_dev" => 273.63253429178945,
+          "std_dev_ips" => 1222.5815257169884,
+          "std_dev_ratio" => 0.5783926704759165,
+          "run_times" => []
+        },
+        "insert_pg/insert_changeset" => %{
+          "average" => 465.8669101807624,
+          "ips" => 2146.5357984150173,
+          "maximum" => 13092.0,
+          "median" => 452.0,
+          "minimum" => 338.0,
+          "mode" => 440.0,
+          "percentiles" => %{"50" => 452.0, "99" => 638.0},
+          "sample_size" => 10677,
+          "std_dev" => 199.60367678670747,
+          "std_dev_ips" => 919.6970816229071,
+          "std_dev_ratio" => 0.4284564377179282,
+          "run_times" => []
+        }
+      }
     }
   end
 end
