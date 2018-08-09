4d84c81059b5a40b08bad9afb0024af2246d7cba
Tallys Martins <tallysmartins@gmail.com>
2018-08-09 17:09:06 -0300

    Change status of existent jobs to success
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- priv/repo/migrations/20180809140205_add_exit_status_to_job.exs
+++ priv/repo/migrations/20180809140205_add_exit_status_to_job.exs
@@ -5,5 +5,10 @@ defmodule ElixirBench.Repo.Migrations.AddExitStatusToJob do
     alter table(:jobs) do
       add :exit_status, :integer, default: nil
     end
+
+    # assume all existent jobs has finished with success
+    execute """
+    UPDATE jobs SET exit_status = 0 WHERE exit_status IS NULL;
+    """, "" # nothing on down
   end
 end
